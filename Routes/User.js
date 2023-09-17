3b2aa7808963cacbc7cc5d7add7a3b34689d40a7
golamrabbani93 <golamrabbani9373@gmail.com>
2023-09-17 16:00:18 +0600

    check user email already in database

--- Routes/User.js
+++ Routes/User.js
@@ -12,15 +12,28 @@ router.get('/:id', async (req, res) => {});
 router.post('/', async (req, res) => {
 	try {
 		const user = req.body;
-		const checkUser = new userCollection(user);
-		const savedUser = await checkUser.save();
-		if (savedUser._id) {
-			res.status(200).send({
-				message: 'User Inserted',
-			});
+
+		const userQuery = {
+			email: user.email,
+		};
+		//! Check user Already in database
+		const getUser = await userCollection.find(userQuery);
+
+		if (!getUser[0]) {
+			const newUser = new userCollection(user);
+			const savedUser = await newUser.save();
+			if (savedUser._id) {
+				res.status(200).send({
+					message: 'User Inserted',
+				});
+			} else {
+				res.status(404).send({
+					message: 'User Inserted Faild',
+				});
+			}
 		} else {
-			res.status(404).send({
-				message: 'User Inserted Faild',
+			res.status(200).send({
+				message: 'User already in Database',
 			});
 		}
 	} catch (error) {
