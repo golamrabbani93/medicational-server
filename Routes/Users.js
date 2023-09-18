696925cd3943907ad04fdb9d89eff14e96a06a23
golamrabbani93 <golamrabbani9373@gmail.com>
2023-09-18 21:12:57 +0600

    get user role

--- Routes/Users.js
+++ Routes/Users.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 // !Import Schema
 const userSchema = require('../Schemas/userSchema');
+const verifyJWT = require('../config/JwtConfig');
 
 // !create user Collection
 const userCollection = mongoose.model('user', userSchema);
-router.get('/', async (req, res) => {
+router.get('/', verifyJWT, async (req, res) => {
 	try {
 		const users = await userCollection.find({});
 		if (users.length > 0) {
@@ -57,8 +58,35 @@ router.post('/', async (req, res) => {
 		res.status(500).send('There Was Server Side Error');
 	}
 });
-router.put('/admin/:id', async (req, res) => {
+router.get('/admin/:email', async (req, res) => {
 	try {
+		const email = req.params.email;
+		const query = {email};
+		const user = await userCollection.findOne(query);
+
+		if (user.role === 'Admin') {
+			res.status(200).send({
+				Admin: true,
+			});
+		} else {
+			res.status(404).send({
+				Admin: false,
+			});
+		}
+	} catch (error) {
+		res.status(500).send('There Was Server Side Error');
+	}
+});
+router.put('/admin/:id', verifyJWT, async (req, res) => {
+	try {
+		//! check user Admin or Not
+		const decodedEmail = req.decoded.email;
+		const admin = await userCollection.findOne({email: decodedEmail});
+		if (admin.role !== 'Admin') {
+			return res.status(403).send({
+				message: 'Make Admin Faild',
+			});
+		}
 		const userId = req.params.id;
 		const query = {_id: userId};
 		const user = await userCollection.findOne(query);
@@ -73,7 +101,7 @@ router.put('/admin/:id', async (req, res) => {
 				message: 'Make Admin Successful',
 			});
 		} else {
-			res.status(404).send({
+			res.status(403).send({
 				message: 'Make Admin Faild',
 			});
 		}
