98217f071eceeceb54ab36333fb702cc56ce9b7b
golamrabbani93 <golamrabbani9373@gmail.com>
2023-09-20 18:12:59 +0600

    get all doctors Complete

--- Routes/Doctor.js
+++ Routes/Doctor.js
@@ -11,6 +11,25 @@ const appointmentSchema = require('../Schemas/appointmentSchema');
 const doctorCollection = mongoose.model('doctor', doctorSchema);
 const AppointmentCollection = mongoose.model('appointmentoption', appointmentSchema);
 
+router.get('/', verifyJWT, async (req, res) => {
+	try {
+		const qurey = {};
+		const allDoctors = await doctorCollection.find(qurey);
+		if (allDoctors[0]) {
+			res.status(200).send({
+				message: 'Successful',
+				data: allDoctors,
+			});
+		} else {
+			res.status(404).send({
+				message: 'Data Not Found',
+				data: 0,
+			});
+		}
+	} catch (error) {
+		res.status(500).send('There was Sever Side Error');
+	}
+});
 router.post('/', verifyJWT, async (req, res) => {
 	try {
 		const doctorData = req.body;
